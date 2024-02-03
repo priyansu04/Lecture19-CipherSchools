1177cdd1ffa5fcd587f3c8ddbf6ad89b62f432ee
priyansu04 <91240406+priyansu04@users.noreply.github.com>
2024-02-03 16:23:04 +0530

    Add files via upload

---
+++ JaggedArrays.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class JaggedArrays {
+    public static void main(String[]args) {
+
+        int arr[][] = new int[3][];
+        arr[0]=new int[3];
+        arr[1]=new int[2];
+        arr[2]=new int[3];
+    }
+}
---
+++ MultiDimArrays.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+public class MultiDimArrays {
+    public static void main(String[]args){
+        int rows =5;
+        int cols =3;
+        int arr[][]=new int[rows][cols];
+        for (int i=0;i< arr.length;i++)
+        {
+            for ( int j=0;j< arr[0].length;j++)
+            {
+                System.out.println(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("****************************************************");
+    int arr2[][]={{1,2,3},
+            {4,5,6},
+            {7,8,9}
+            };
+        for (int i=0;i< arr2.length;i++)
+        {
+            for ( int j=0;j< arr2[0].length;j++)
+            {
+                System.out.println(arr2[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
